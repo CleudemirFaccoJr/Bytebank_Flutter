61d22258502b4bba2dae8bb3350b00ae4e1f04ad
CleudemirFaccoJr <cleudemirfaccojr@gmail.com>
2025-08-28 10:20:26 -0300

    Mais correções iniciais

---
+++ bytebank/lib/app_colors.dart
@@ -0,0 +1,7 @@
+ import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Colors.white;
+  static const Color shadow = Colors. black12;
+  static const Color error = Colors.red;
+}
