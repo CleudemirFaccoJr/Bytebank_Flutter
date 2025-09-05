2b54f6cddf51e80cdfb13819d8aae3bc9492f906
CleudemirFaccoJr <cleudemirfaccojr@gmail.com>
2025-09-05 15:38:40 -0300

    REVISAR
    - Esqueci minha Senha: esta tela foi criada, está sem lógica nenhuma para a redefinição de senha;
    
    - Login: esta tela está com a implementação da lógica de login, mas a mesma não funciona (pelo menos não na THS);
    
    - Main.dart: algumas coisas foram modificadas.
    
    -Dashboard_screen: ta criada, sem nada, mas apenas pra testar a login screen. Agora ela tem o NavigationBar embaixo. Funciona? Não sei. mas já to adiantando.

--- bytebank/lib/screens/dashboard_screen.dart
+++ bytebank/lib/screens/dashboard_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int currentPageIndex = 0;
+
+  final List<Widget> pages = [
+    Center(child: Text("Início")),
+    Center(child: Text("Transferências")),
+    Center(child: Text("Investimentos")),
+    Center(child: Text("Perfil")),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +23,36 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dashboard"),
       ),
-      body: const Center(
-        child: Text("Bem-vindo à Dashboard!"),
+      body: pages[currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Início',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.monetization_on),
+            icon: Icon(Icons.monetization_on_outlined),
+            label: 'Transferências',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.graphic_eq_rounded),
+            icon: Icon(Icons.graphic_eq_rounded),
+            label: 'Investimentos',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outlined),
+            label: 'Perfil',
+          ),
+        ],
       ),
     );
   }
